6974e9da5bac0a40709a10f9f859ec3f4b454d65
vuongnhattin <tinvuongnhat@gmail.com>
2023-05-23 23:46:11 +0700

    nothing

--- module-4/module4-solution/SpeakGoodBye.js
+++ module-4/module4-solution/SpeakGoodBye.js
@@ -20,7 +20,6 @@ var speakWord = "Good Bye";
 byeSpeaker.speak = function (name) {
   console.log(speakWord + " " + name);
 }
-
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
